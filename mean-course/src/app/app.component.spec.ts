e199fe256ac0833a231e1d9429d69c88795f8058
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-18 21:50:07 +0530

    mean project setup

--- mean-course/src/app/app.component.spec.ts
+++ mean-course/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '01-my-angular' title`, () => {
+  it(`should have the 'mean-course' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('01-my-angular');
+    expect(app.title).toEqual('mean-course');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 01-my-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mean-course');
   });
 });
--- mean-course/src/app/app.component.ts
+++ mean-course/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1 class="text-red-500">Hello world! {{ title }}</h1> `,
   standalone: true,
   imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '01-my-angular';
+  title = 'mean-course';
 }
